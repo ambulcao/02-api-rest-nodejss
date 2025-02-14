fd4d06901f5c742e4c3e04f5621f6348dc71214d
Alexandre Marques Bulcao <ambulcao@gmail.com>
2025-02-14 14:51:55 -0400

    fix:prefix transactions

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
-import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
 app.register(transactionsRoutes, {
   prefix: 'transactions'
+  //prefix: 'transactions'
 })
 
 app.listen({ 
