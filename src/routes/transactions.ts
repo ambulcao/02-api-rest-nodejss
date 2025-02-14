560732522605fac95a23f18204ee7915e8ddc3ee
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2025-02-14 15:39:25 -0400

    fix:route transactions randomUUID (#7)
    * fix:commit srv/server.ts
    
    * fix:transactions randomUUID

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,11 @@
-import { knex } from "../database"
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
-import crypto from "node:crypto"
+import { randomUUID } from "node:crypto"
+import { knex } from "../database"
 
 export async function transactionsRoutes(app: FastifyInstance) {
     app.post('/', async (request, reply) => {
+
         const createTransactionBodySchema = z.object({
             title: z.string(),  
             amount: z.number(),
@@ -15,7 +16,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const transactions = await knex('transactions')
             .insert({
-                id: crypto.randomUUID(),
+                id: randomUUID(),
                 title,
                 amount: type === 'credit' ? amount : amount * -1,
             })
