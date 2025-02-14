a7905b9c214c53b5962937c82dfd66c53585434d
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2025-02-14 14:46:37 -0400

    Node/api rest (#3)
    * feat:create pluggin fastify
    
    * feat:create transactions

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,19 @@ import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
+})
 
 app.listen({ 
-  port: env.PORT, 
+  port: env.PORT,
+  host: '0.0.0.0',
 })
 .then(() => {
   console.log('HTTP server running!')
+})
+.catch((err) => {
+  console.error('Error starting server:', err)
+  process.exit(1)
 })
  
